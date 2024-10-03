4c1ddb468e5469916d62e3d83bb2cf3d7c8e58ee
Amzz-Maxxing <adham_22010261@utp.edu.my>
2024-10-03 20:15:33 +0800

    Add files via upload
    Added the necessary class based on UML class diagram

---
+++ OOP_Algebro_HR_Management_Website/TimeTrackingList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Algebro_HR_Management_Website
+{
+    internal class TimeTrackingList
+    {
+    }
+}
