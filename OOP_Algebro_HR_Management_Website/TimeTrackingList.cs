24fd7de6376a590139b3e6b12c51b1f0edc4be8a
nowkeekun <ikhwan_22010398@utp.edu.my>
2024-10-05 23:44:50 +0800

    Update1 TimeTrackingList.cs

--- OOP_Algebro_HR_Management_Website/TimeTrackingList.cs
+++ OOP_Algebro_HR_Management_Website/TimeTrackingList.cs
@@ -8,9 +8,32 @@ namespace OOP_Algebro_HR_Management_Website
 {
     internal class TimeTrackingList
     {
-        public List<TimeTracking> timeTrackingList = new List<TimeTracking>();
-
         public int totalHour { get; set;}
+        public List<TimeTracking> timeTrackings;
+
+        public TimeTrackingList()
+        {
+          timeTrackings = new List<TimeTracking>();
+        }
+
+        public void addTimeTracking(TimeTracking timeTracking)
+        {
+          timeTrackings.Add(timeTracking)
+        }
+
+        public void removeTimeTracking(TimeTracking timeTracking)
+        {
+          timeTrackings.Remove(timeTracking)
+        }
+
+        public void display()
+        {
+          foreach (TimeTracking timeTracking in timeTrackings)
+          {
+              Console.WriteLine("Start Time: " + timeTracking.startTime);
+              Console.WriteLine("End Time: " + timeTracking.endTime);
+          }
+        }
 
         public void totalHourList ()
         {
