4224bf842492cf746c47eb0a3381e09dbb0ad9e7
Amzz-Maxxing <adham_22010261@utp.edu.my>
2024-10-05 19:45:29 +0800

    Update Program.cs
    Added new ApplicationList, EmployeeList, FeedbackList, KPI, LeaveRequest, TimeTrackingList

--- OOP_Algebro_HR_Management_Website/Program.cs
+++ OOP_Algebro_HR_Management_Website/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Algebro_HR_Management_Website
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            ApplicantList applicantList = new ApplicantList();
+
+            EmployeeList employeeList = new EmployeeList();
+
+            FeedbackList feedbackList = new FeedbackList();
+
+            KPI kPI = new KPI();
+
+            LeaveRequest leaveRequest = new LeaveRequest();
+
+            TimeTrackingList timeTrackingList = new TimeTrackingList();
+        }
+    }
+}
