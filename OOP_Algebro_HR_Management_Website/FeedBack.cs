bd67925dbaa311ea24b7512c77e64130a9ed80e0
nowkeekun <ikhwan_22010398@utp.edu.my>
2024-10-05 23:17:44 +0800

    Update1 FeedBack.cs

--- OOP_Algebro_HR_Management_Website/FeedBack.cs
+++ OOP_Algebro_HR_Management_Website/FeedBack.cs
@@ -8,7 +8,11 @@ namespace OOP_Algebro_HR_Management_Website
 {
     internal class FeedBack
     {
-        public int ID { get; set; }
+        public int id { get; set; }
         public string feeback { get; set; }
+        public void AddFeedback()
+        {
+            //Console.WriteLine("Feedback: " + feedback)
+        }
     }
 }
