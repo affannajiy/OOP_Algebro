9359923c736843959d7e42811b9b129d5d5faca5
Amzz-Maxxing <adham_22010261@utp.edu.my>
2024-10-05 14:04:30 +0800

    Update Date.cs
    added public Date(...)

--- OOP_Algebro_HR_Management_Website/Date.cs
+++ OOP_Algebro_HR_Management_Website/Date.cs
@@ -11,5 +11,11 @@ namespace OOP_Algebro_HR_Management_Website
         public int Day { get; set; }
         public int Month { get; set; }
         public int Year { get; set; }
+        public Date(string day, string month, string year)
+        {
+            this.day = Day;
+            this.month = Month;
+            this.year = Year;
+        }
     }
 }
