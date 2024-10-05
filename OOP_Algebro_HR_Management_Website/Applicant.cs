550fb5bd5b6ad5be72b2d0ea7fd48fe46c91501a
Affan Najiy <affannajiy@gmail.com>
2024-10-05 11:03:13 +0800

    Update Applicant.cs
    Updated based on UML Diagram

--- OOP_Algebro_HR_Management_Website/Applicant.cs
+++ OOP_Algebro_HR_Management_Website/Applicant.cs
@@ -8,5 +8,14 @@ namespace OOP_Algebro_HR_Management_Website
 {
     internal class Applicant
     {
+        public Date ivDate = new Date();
+        public string appName {get; set;}
+        public int appContactNum {get; set;}
+        public string appRole {get; set;}
+
+        public void ApplicantInfo(string appName, int appContactNum, string appRole)
+        {
+
+        }
     }
 }
