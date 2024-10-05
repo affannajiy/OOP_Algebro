1f031e65ab50cfa2449f26882c54690a980bb87c
Affan Najiy <affannajiy@gmail.com>
2024-10-05 10:55:06 +0800

    Employee Update
    Added according to UML

--- OOP_Algebro_HR_Management_Website/Employee.cs
+++ OOP_Algebro_HR_Management_Website/Employee.cs
@@ -8,5 +8,20 @@ namespace OOP_Algebro_HR_Management_Website
 {
     internal class Employee
     {
+        public string employeeName { get; set; }
+        public int employeeID { get; set; }
+        public string employeePosition { get; set; }
+        public int employeeContactNum { get; set; }
+        public string employeeStatus { get; set; }
+
+        public void EmployeeInfo
+            (string employeeName, int employeeID, string employeePosition, int employeeContactNum, string employeeStatus)
+        {
+            //Console.WriteLine("Employee Name: " + employeeName);
+            //Console.WriteLine("Employee ID: " + employeeID);
+            //Console.WriteLine("Employee Position: " + employeePosition);
+            //Console.WriteLine("Employee Contact Number: " + employeeContactNum);
+            //Console.WriteLine("Employee Status: " + employeeStatus);
+        }
     }
 }
