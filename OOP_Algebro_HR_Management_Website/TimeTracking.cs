dacb3185afca5158708c041291edd80a31b4ff54
ImanHazimInformationSystems <130266573+ImanHazimInformationSystems@users.noreply.github.com>
2024-10-05 16:17:06 +0800

    Updated TimeTracking.cs

--- OOP_Algebro_HR_Management_Website/TimeTracking.cs
+++ OOP_Algebro_HR_Management_Website/TimeTracking.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,16 @@ namespace OOP_Algebro_HR_Management_Website
 {
     internal class TimeTracking
     {
+        public string employeeName { get; set; }
+        public int employeeID { get; set; }
+
+        public int startTime { get; set; }
+
+        public int endTime { get; set; }
+
+        public void totalHour()
+        {
+
+        }
     }
 }
