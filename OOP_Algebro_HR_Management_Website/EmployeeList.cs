d67640f1b9dda8e470f0ebcd2ec109a16c6dba42
Affan Najiy <affannajiy@gmail.com>
2024-10-05 11:00:08 +0800

    Update EmployeeList.cs
    Added based on UML Diagram

--- OOP_Algebro_HR_Management_Website/EmployeeList.cs
+++ OOP_Algebro_HR_Management_Website/EmployeeList.cs
@@ -8,5 +8,38 @@ namespace OOP_Algebro_HR_Management_Website
 {
     internal class EmployeeList
     {
+        public List<Employee> employeeList = new List<Employee>();
+
+        public EmployeeList()
+        {
+
+        }
+
+        public void AddEmployee()
+        {
+            /*
+            Console.WriteLine("Add Employee");
+            Console.WriteLine("Enter Employee Name: ");
+            string employeeName = Console.ReadLine();
+            Console.WriteLine("Enter Employee ID: ");
+            int employeeID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Position: ");
+            string employeePosition = Console.ReadLine();
+            Console.WriteLine("Enter Employee Contact Number: ");
+            int employeeContactNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Status: ");
+            string employeeStatus = Console.ReadLine()
+            */
+        }
+
+        public void RemoveEmployee()
+        {
+
+        }
+
+        public void DisplayEmployeeList()
+        {
+
+        }
     }
 }
