664695d019df206f08e0da792136ad4295053d8d
Amzz-Maxxing <adham_22010261@utp.edu.my>
2024-10-05 14:12:27 +0800

    Update Applicant.cs
    added Date()

--- OOP_Algebro_HR_Management_Website/Applicant.cs
+++ OOP_Algebro_HR_Management_Website/Applicant.cs
@@ -8,14 +8,21 @@ namespace OOP_Algebro_HR_Management_Website
 {
     internal class Applicant
     {
-        public Date ivDate = new Date();
-        public string appName {get; set;}
-        public int appContactNum {get; set;}
-        public string appRole {get; set;}
+        public string AppName { get; set; }
+        
+        public int AppContactNumber { get; set; }
 
-        public void ApplicantInfo(string appName, int appContactNum, string appRole)
+        public string AppRole { get; set; }
+
+        public Date IvDate = new Date();
+        
+        public Position CurrPosition { get; set; }
+
+        public Applicant(string name, int contactNumber, string role, Date ivDate)
         {
-
+            AppName = name; AppContactNumber = contactNumber; AppRole = role; IvDate = ivDate;
         }
+
+        public Applicant() { }
     }
 }
