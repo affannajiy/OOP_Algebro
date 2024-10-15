39c793957b4e4d2642c812124e687a7d91215160
Affan Najiy <affannajiy@gmail.com>
2024-10-15 12:58:44 +0800

    Misc

--- OOP_Algebro_HR_Management_Website/obj/Debug/net8.0/HRMangement_Main.AssemblyInfo.cs
+++ OOP_Algebro_HR_Management_Website/obj/Debug/net8.0/HRMangement_Main.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMangement_Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ff1e03d15f551d01109d6df7513de67ccbff2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+507ce0471ca939ca88ca7396a1852402d1670dfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMangement_Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMangement_Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
